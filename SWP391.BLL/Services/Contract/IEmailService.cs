3ce82f624e611f5080623512ab94a54c101aad6b
letranminhdat1516 <datltmse170095@fpt.edu.vn>
2024-06-27 01:28:18 +0700

    DAT|updateVoucherChatLogin|2706

---
+++ SWP391.BLL/Services/Contract/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string email, string subject, string message);
+    Task SendEmailsAsync(List<string> emails, string subject, string message);
+}
