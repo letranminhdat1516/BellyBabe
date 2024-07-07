0ffa706dbf1bb0bab9ce37617e7fd7ed31677af2
Lê Trần Minh Đạt <94780550+letranminhdat1516@users.noreply.github.com>
2024-07-07 14:39:17 +0700

    DAT|updateTokenStringee|07071439

--- SWP391.APIs/obj/Debug/net8.0/SWP391.APIs.AssemblyInfo.cs
+++ SWP391.APIs/obj/Debug/net8.0/SWP391.APIs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391.APIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd42114a5e09c855553aa54172a10c5a3f9632a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391.APIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391.APIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
